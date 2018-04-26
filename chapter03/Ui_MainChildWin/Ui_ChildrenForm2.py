d22e999a0378a0b61ec001048e9b6cc1daa10549
yergen <15010908626@163.com>
2018-04-26 10:48:49 +0800

    在主窗口加载其他窗口

---
+++ chapter03/Ui_MainChildWin/Ui_ChildrenForm2.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'E:\pyqt5\ChildrenForm2.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(550, 528)
+        self.textEdit = QtWidgets.QTextEdit(Form)
+        self.textEdit.setGeometry(QtCore.QRect(0, 0, 551, 531))
+        self.textEdit.setObjectName("textEdit")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.textEdit.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">我是子窗口的内容</p></body></html>"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
---
+++ chapter03/Ui_MainChildWin/Ui_MainForm2.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'E:\pyqt5\MainForm2.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralWidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, 801, 551))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.MaingridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.MaingridLayout.setSizeConstraint(QtWidgets.QLayout.SetNoConstraint)
+        self.MaingridLayout.setContentsMargins(0, 0, 0, 0)
+        self.MaingridLayout.setObjectName("MaingridLayout")
+        MainWindow.setCentralWidget(self.centralWidget)
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 800, 23))
+        self.menuBar.setObjectName("menuBar")
+        self.menu = QtWidgets.QMenu(self.menuBar)
+        self.menu.setObjectName("menu")
+        self.menu_2 = QtWidgets.QMenu(self.menuBar)
+        self.menu_2.setObjectName("menu_2")
+        MainWindow.setMenuBar(self.menuBar)
+        self.toolBar = QtWidgets.QToolBar(MainWindow)
+        self.toolBar.setObjectName("toolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        self.statusBar = QtWidgets.QStatusBar(MainWindow)
+        self.statusBar.setObjectName("statusBar")
+        MainWindow.setStatusBar(self.statusBar)
+        self.fileOpenAction = QtWidgets.QAction(MainWindow)
+        self.fileOpenAction.setObjectName("fileOpenAction")
+        self.fileNewAction = QtWidgets.QAction(MainWindow)
+        self.fileNewAction.setObjectName("fileNewAction")
+        self.fileCloseAction = QtWidgets.QAction(MainWindow)
+        self.fileCloseAction.setObjectName("fileCloseAction")
+        self.addWinAction = QtWidgets.QAction(MainWindow)
+        self.addWinAction.setCheckable(False)
+        self.addWinAction.setObjectName("addWinAction")
+        self.menu.addAction(self.fileOpenAction)
+        self.menu.addAction(self.fileNewAction)
+        self.menu.addAction(self.fileCloseAction)
+        self.menuBar.addAction(self.menu.menuAction())
+        self.menuBar.addAction(self.menu_2.menuAction())
+        self.toolBar.addAction(self.addWinAction)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menu.setTitle(_translate("MainWindow", "文件(&F)"))
+        self.menu_2.setTitle(_translate("MainWindow", "编辑(&E)"))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
+        self.fileOpenAction.setText(_translate("MainWindow", "打开"))
+        self.fileOpenAction.setToolTip(_translate("MainWindow", "打开"))
+        self.fileOpenAction.setShortcut(_translate("MainWindow", "Alt+O"))
+        self.fileNewAction.setText(_translate("MainWindow", "新建"))
+        self.fileNewAction.setToolTip(_translate("MainWindow", "新建"))
+        self.fileNewAction.setShortcut(_translate("MainWindow", "Alt+N"))
+        self.fileCloseAction.setText(_translate("MainWindow", "关闭"))
+        self.fileCloseAction.setToolTip(_translate("MainWindow", "关闭"))
+        self.fileCloseAction.setShortcut(_translate("MainWindow", "Alt+C"))
+        self.addWinAction.setText(_translate("MainWindow", "添加窗体"))
+        self.addWinAction.setToolTip(_translate("MainWindow", "添加窗体"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
