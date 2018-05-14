80dbf0e7f9e6f27c49a78dfa8bb2ebf8749c59ef
yergen <15010908626@163.com>
2018-05-14 16:22:16 +0800

    显示气泡提示信息

---
+++ chapter04/qt404_QToolTip.py
@@ -0,0 +1,23 @@
+#-*- coding: UTF-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QToolTip
+from PyQt5.QtGui import QFont
+
+class Winform(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+    
+    def initUI(self):
+        QToolTip.setFont(QFont('SansSerif', 10))
+        self.setToolTip('这是一个<b>气泡提示</b>')
+        self.setGeometry(200, 300, 400, 400)
+        self.setWindowTitle('气泡提示demo')
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Winform()
+    win.show()
+    sys.exit(app.exec_())
+    
