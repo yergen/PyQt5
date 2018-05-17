d5a1b969229660e9dda0704b814da1565e27ea19
yergen <15010908626@163.com>
2018-05-17 16:09:45 +0800

    QInputDialog的使用

---
+++ chapter04/qt04_QInputDialog.py
@@ -0,0 +1,52 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class InputdialogDemo(QWidget):
+    def __init__(self, parent=None):
+        super(InputdialogDemo, self).__init__(parent)
+        self.setWindowTitle("Input Dialog例子")
+        layout = QFormLayout()
+        
+        self.btn1 = QPushButton("获得列表里的选项")
+        self.btn1.clicked.connect(self.getItem)
+        self.le1 = QLineEdit()
+        layout.addRow(self.btn1, self.le1)
+        
+        self.btn2 = QPushButton("获得字符串")
+        self.btn2.clicked.connect(self.getText)
+        self.le2 = QLineEdit()
+        layout.addRow(self.btn2, self.le2)
+        
+        self.btn3 = QPushButton("获得整数")
+        self.btn3.clicked.connect(self.getInt)
+        self.le3 = QLineEdit()
+        layout.addRow(self.btn3, self.le3)
+        self.setLayout(layout)
+        
+    def getItem(self):
+        items = ("C", "C++", "Java", "Python")
+        item, ok = QInputDialog.getItem(self, "select input dialog", \
+        "语言列表", items, 0, False)
+        if ok and item:
+            self.le1.setText(item)
+    
+    def getText(self):
+        text, ok = QInputDialog.getText(self, "Text input dialog", \
+        "输入姓名：")
+        if ok:
+            self.le2.setText(str(text))
+    
+    def getInt(self):
+        num, ok = QInputDialog.getInt(self, "integet input dialog", \
+        "输入数字：")
+        if ok:
+            self.le3.setText(str(num))
+            
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demo = InputdialogDemo()
+    demo.show()
+    sys.exit(app.exec_())
+        
